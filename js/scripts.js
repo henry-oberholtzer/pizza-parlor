4138d8d9a3773c391a5d0d2310fbaba90f19154b
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-03 09:00:31 -0700

    Pizza.prototype.cost passes all tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-function Pizza(toppings, base, size) {
+function Pizza(toppings, baseSauce, baseCheese, size) {
     this.toppings = toppings;
-    this.base = base;
+    this.baseSauce = baseSauce;
+    this.baseCheese = baseCheese;
     this.size = size;
 }
 
@@ -11,8 +12,24 @@ Pizza.prototype.cost = function() {
         large: 12,
         xlarge: 14,
     };
+    const baseSauce = {
+        none: 1,
+        marinara: 3,
+        whitesauce: 3,
+        pesto: 4,
+    };
+    const baseCheese = {
+        none: 1,
+        mozzarella: 3,
+        fourcheese: 4,
+        vegan: 4,
+    }
     const orderedSize = this.size;
+    const orderedSauce = this.baseSauce;
+    const orderedCheese = this.baseCheese;
+    const toppingCost = this.toppings.length * 2;
     const sizeCost = size[orderedSize];
-    const numToppings = this.toppings.length;
-    return sizeCost + (numToppings * 2);
+    const sauceCost = baseSauce[orderedSauce];
+    const cheeseCost = baseCheese[orderedCheese];
+    return sizeCost + sauceCost + cheeseCost + toppingCost;
 }
