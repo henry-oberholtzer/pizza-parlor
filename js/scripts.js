1fcc44701de5931c29e9afc9dda4edfe2332dcb3
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-03 08:43:12 -0700

    Pizza.prototype.cost passes first test

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ function Pizza(toppings, base, size) {
     this.base = base;
     this.size = size;
 }
+
+Pizza.prototype.cost = function() {
+    const sizeCost = {
+        small: 8,
+        medium: 10,
+        large: 12,
+        xlarge: 14,
+    };
+    const orderedSize = this.size;
+    return sizeCost[orderedSize];
+}
